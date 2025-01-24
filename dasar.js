f3549bedc1bc2363a9ea1d83dfb26ff29463b180
gilang danang <gdanangsantana@gmail.com>
2025-01-24 14:29:38 +0700

    upload pertama

---
+++ dasar.js
@@ -0,0 +1,51 @@
+// Kamu diminta untuk memprogram suatu game sederhana, Proxytia namanya. 
+// Untuk memulai game itu diperlukan 2 variabel (untuk sekarang), 
+// yaitu nama dan peran. Variabel peran harus memiliki isi data, 
+// bila nama kosong, pemain akan diberikan peringatan berupa "nama wajib diisi"
+// bila kosong pemain akan diberikan peringatan berupa "Pilih Peranmu untuk memulai game". 
+// Terdapat 3 peran berbeda yaitu Ksatria, Tabib, dan Penyihir. 
+// Tugas Anda adalah untuk membuat program yang mengecek isi variabel 
+// peran serta mengeluarkan respon sesuai isi variabel tersebut.
+//ada 4 jenis respons sesuai dengan 3 jenis peran yaitu
+// halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!
+// halo Tabib ${nama} , kamu akan membantu temanmu yang terluka
+// halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!
+// tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada
+//tips belajar penggunaan `` (backtick) pada javascript agar
+//mudah dalam memasukan variabel ke dalam string
+//tapi tanpa backtick juga ga masalah sih yg penting output sesuai
+
+// algoritma
+//isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
+
+// kamu akan bermain game Proxitya 
+// kamu akan mengisi dua variabel nama dan peran
+// masing masing harus diisi jika tidak maka akan keluar peringatan untuk mengisinya
+// nama kamu akan muncul ketika kamu sudah mengisinya 
+// kamu harus memilih satu dari tiga peran Ksatria, Tabib, dan Penyihir
+// jika kamu memilih peran selain yang diatas, maka kamu jadi bot aja
+
+//code disini gunakan console.log untuk outputnya
+function Proxytia() {
+let nama = "agung", peran = "Tabib";
+
+if (!nama) {
+    console.log("nama wajib di isi!!")
+} else {
+    console.log(nama);
+}
+
+if (!peran) {
+    console.log("Pilih peranmu untuk memulai game!!")
+} else if (peran == "Ksatria") {
+    console.log(`halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!`)
+} else if (peran == "Tabib") {
+    console.log(`halo Tabib ${nama} , kamu akan membantu temanmu yang terluka`)
+} else if (peran == "Penyihir") {
+    console.log(`halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!`)
+}    else {
+    console.log(`hai ${nama} , tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada`)``
+} return;
+}
+
+Proxytia();
